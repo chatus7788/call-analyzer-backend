34c7aa7a28a63ffe4fffcd45ca738dc0469a9dbc
Nitish Nashine <nitishnashine@gmail.com>
2025-12-13 22:38:23 +0530

    fix: update upload middleware path for audio transcription

--- routes/transcription.routes.js
+++ routes/transcription.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const uploadAudio = require("../middlewares/upload.middleware");
+const uploadAudio = require("../middlewares/uploadS3.middleware");
 const { startTranscription } = require("../controllers/transcription.controller");
 
 router.post("/transcribe", uploadAudio.single("audio"), startTranscription);
